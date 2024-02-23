d780638806f09d9ebd10696fe5fefe658fc1ce37
Diego <145619634+diegoxavir@users.noreply.github.com>
2024-02-23 00:18:02 -0500

    fixed first bug
    Fixed the bug that pieces would stack on one another.

--- js/main.js
+++ js/main.js
@@ -25,6 +25,14 @@ function changeBGImage() {
 	// and updating the background-image style of the puzzle board element.
 
 	// bug fix #2 should go here. it's at most 3 lines of JS code.
+	//puzzlePieces
+	//if (draggedPiece) {
+	//	return
+	//}
+	//if (draggedPiece) {
+	//
+	//	console.log('zone already taken');
+	//}
 	puzzleBoard.style.backgroundImage = `url(images/backGround${this.id}.jpg)`;
 }
 
@@ -35,28 +43,42 @@ function handleStartDrag() {
 	draggedPiece = this;
 }
 
-function handleDragOver(e) { 
-	e.preventDefault(); // e is shorthand for event
-	// this overrides the default dragover behaviour
-	console.log('dragged over me'); 
+function handleDragOver(event) {
+	event.preventDefault();
+	console.log('dragged over a zone');
 }
 
-function handleDrop(e) { 
-	e.preventDefault();
-	console.log('dropped something on me');
-	// bug fix #1 should go here, and it's at most 3 lines of JS code
+function handleDrop(event) {
+	event.preventDefault();
+	console.log('dropped on the zone')
+	
+//	if (puzzlePieces ) 
+//	if (handleDrop) {
+//		draggedPiece = null;
+//	}
+	//if (handleDrop) {
+	//	return;
+	//}
+//	if (child) {
+//	return
+//	}
+//	if (this.child > 0) {
+//		return;
+///	}
 
-	// this line is going to move the dragged piece from the left side of the board
-	// into whatever drop zone we choose. appendChild means "add element to the container"
-	this.appendChild(draggedPiece);
+//	if (firstChild > 0) {
+//		return;
+//	}
+//	if (this.firstElementChild > 0 ) {
+//		return;
+//	}
+	if (this.firstElementChild) {
+		return;
+	}
+//bug fix here-check condition
+
+this.appendChild(draggedPiece)
 }
-// step 2
-// event handling always goes at the bottom => 
-// how do we want users to interact with our app
-
-// 1 to 1 event handling
-//theButton.addEventListener("click", changeBGImage);
-
 // 1 to many event handling
 // add event handling to each button in the collection of buttons, one at a time
 theButtons.forEach(button => button.addEventListener("click", changeBGImage));
@@ -64,8 +86,7 @@ theButtons.forEach(button => button.addEventListener("click", changeBGImage));
 // add the drag event handling to the puzzle pieces
 puzzlePieces.forEach(piece => piece.addEventListener("dragstart", handleStartDrag));
 
-// add the dragover AND the drop event handling to the drop zones
-dropZones.forEach(zone => zone.addEventListener("dragover", handleDragOver));
+dropZones.forEach(zone => zone.addEventListener('dragover', handleDragOver));
 
-// add the drop event handling
-dropZones.forEach(zone => zone.addEventListener("drop", handleDrop));
+dropZones.forEach(zone => zone.addEventListener('drop', handleDrop));
+
